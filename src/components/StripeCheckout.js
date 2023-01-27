840722626ed56ee71acfffa6b348603437b41680
Adeyunze <adeyunze05@gmail.com>
2023-01-27 17:11:45 +0000

    Changed logs

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -47,9 +47,8 @@ const CheckoutForm = () => {
         JSON.stringify({cart, shipping_fee, total_amount})
         )
         setClientSecret(data.clientSecret)
-        console.log(data.clientSecret);
     } catch (error) {
-      console.log(error.response);
+      // console.log(error.response);
     }
   }
 
