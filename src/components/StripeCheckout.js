5e9a578eaba967fe25132035e7d6f4c84bbc1be9
Adeyunze <adeyunze05@gmail.com>
2023-01-28 11:19:51 +0000

    Open link in new tab

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -122,7 +122,7 @@ const CheckoutForm = () => {
         <p className={succeeded ? 'result-message': 'result-message hidden'}>
           Payment succeeded see the result in your
           {' '}
-          <a href={`https://dashboard.stripe.com/test/payments`}>
+          <a href={`https://dashboard.stripe.com/test/payments`} target={`_blank`}>
             Stripe dashboard
           </a>
           {' '}
